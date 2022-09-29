114d52e7f85452139b60e76ba2193d4dc51e7c09
nabendu82 <nabendu.biswas@gmail.com>
2022-09-29 17:35:30 +0530

    React Day-2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-// import Counter from './components/Counter';
-import EventBind from './components/EventBind';
-// import { Greet, ReactGreet } from './components/Greet';
-// import Welcome from './components/Welcome';
-// import Greeting from './components/Greeting';
+import ParentLifecycle from './components/ParentLifecycle';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <ReactGreet /> */}
-      {/* <Greeting name="JavaScript">Language of the Web</Greeting>
-      <Greeting name="Golang" />
-      <Greeting name="Swift">Language for iOS app</Greeting>
-      <hr />
-      <Welcome name="Flutter">Language for cross-platform mobile development</Welcome>
-      <Welcome name="React Native" />
-      <Welcome name="Java"><button>Click for Java</button></Welcome> */}
-      <EventBind />
+      <ParentLifecycle />
     </div>
   );
 }
